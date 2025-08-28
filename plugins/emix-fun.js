64802f6c5845a1b5d2714c54d714ba3ca6e9d544
Heatless <dianwilson66@gmail.com>
2025-08-28 16:38:54 +0100

    Update emix-fun.js

--- plugins/emix-fun.js
+++ plugins/emix-fun.js
@@ -31,7 +31,7 @@ cmd({
         let buffer = await getBuffer(imageUrl);
         let sticker = new Sticker(buffer, {
             pack: "Emoji Mix",
-            author: "PK-XMD",
+            author: "MEGA V2",
             type: StickerTypes.FULL,
             categories: ["🤩", "🎉"],
             quality: 75,
