fd79a4906e5f06e15678a7408b729bce9ab69a57
Heatless <dianwilson66@gmail.com>
2025-08-29 01:54:37 +0100

    Update uptime.js

--- plugins/uptime.js
+++ plugins/uptime.js
@@ -24,14 +24,14 @@ cmd({
     },
     message: {
       contactMessage: {
-        displayName: "PKDRILLER | PK-XMD",
-        vcard: `BEGIN:VCARD\nVERSION:3.0\nFN:PKDRILLER | PK-XMD\nORG:PKDRILLER;\nTEL;type=CELL;type=VOICE;waid=254700000000:+254 700 000000\nEND:VCARD`,
+        displayName: "heatless | MEGA V2",
+        vcard: `BEGIN:VCARD\nVERSION:3.0\nFN:MEGA| MEGA V2\nORG:MEGA V2;\nTEL;type=CELL;type=VOICE;waid=254700000000:+254 700 000000\nEND:VCARD`,
         jpegThumbnail: Buffer.alloc(0)
       }
     }
   };
 
-  const uptimeText = `*🤖 PK-XMD Bot Uptime:*\n🕒 ${runtime()}\n\n💡 The bot has been running without interruption.`;
+  const uptimeText = `*🤖 MEGA V2 Bot Uptime:*\n🕒 ${runtime()}\n\n💡 The bot has been running without interruption.`;
 
   await Void.sendMessage(m.chat, {
     text: uptimeText,
@@ -39,17 +39,17 @@ cmd({
       forwardingScore: 999,
       isForwarded: true,
       forwardedNewsletterMessageInfo: {
-        newsletterJid: "120363288304618280@newsletter",
-        newsletterName: "PK-XMD Official"
+        newsletterJid: "120363400223711119@newsletter",
+        newsletterName: "MEGA V2 Official"
       },
       externalAdReply: {
-        title: "PK-XMD",
+        title: "MEGA V2",
         body: "Uptime Monitor by PKDRILLER",
-        thumbnailUrl: "https://files.catbox.moe/fgiecg.jpg",
+        thumbnailUrl: "https://files.catbox.moe/khk52l.jpg",
         mediaType: 1,
         renderLargerThumbnail: true,
         showAdAttribution: true,
-        sourceUrl: "https://github.com/mejjar00254"
+        sourceUrl: "https://github.com/Kelvin12350/MEGA v2"
       }
     }
   }, { quoted: fakeContact });
