8d041c09d756a4aa24d59439a94c0bd059a784e0
Heatless <dianwilson66@gmail.com>
2025-08-29 03:29:42 +0100

    Update rw-tool.js

--- plugins/rw-tool.js
+++ plugins/rw-tool.js
@@ -17,7 +17,7 @@ cmd({
     const { data } = await axios.get(apiUrl);
     
     if (data.status && data.imgUrl) {
-      const caption = `🌌 *Random Wallpaper: ${query}*\n\n> *© Powered by Pkdriller*`;
+      const caption = `🌌 *Random Wallpaper: ${query}*\n\n> *© Powered by MEGA V2*`;
       await conn.sendMessage(from, { image: { url: data.imgUrl }, caption }, { quoted: m });
     } else {
       reply(`❌ No wallpaper found for *"${query}"*.`);
