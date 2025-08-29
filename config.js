62441a152181e10e98d04562bd4e304072051ee2
Heatless <dianwilson66@gmail.com>
2025-08-29 12:46:07 +0100

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "md~S4Rg0BJa#sQlxYNc5oarlhjp-SB3-SZgRpAZKX1vGSouWS4wasRM",
+SESSION_ID: process.env.SESSION_ID || "md~O95znZBC#35FFCneFIK7hBG-FumJ9yqNrGg2f0anCS4P_vyU1pkQ",
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
 AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
